d717a8e39b063f17f238a2f9f35243bde8141858
Eduardo de Paula <eduardo@thunkable.com>
2024-06-07 12:29:49 +0100

    remove test

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
+import { handlePRClosed } from './handlePRClosed';
 import { handlePROpened } from './handlePROpened';
 import { handlePRUpdated } from './handlePRUpdated';
-import { handlePRClosed } from './handlePRClosed';
 
 async function run() {
     try {
@@ -12,24 +12,40 @@ async function run() {
         const initialMessageTemplate = core.getInput('initial-message-template');
         const updateMessageTemplate = core.getInput('update-message-template');
         const closeMessageTemplate = core.getInput('close-message-template');
-        const commitListMessageTemplate = core.getInput('commit-list-message-template');
         const githubToSlackMap = core.getInput('github-to-slack-map');
-        const githubToSlackMapParsed = githubToSlackMap ? JSON.parse(githubToSlackMap) : undefined;
+        const githubToSlackMapParsed = githubToSlackMap
+          ? JSON.parse(githubToSlackMap)
+          : undefined;
 
         const action = github.context.payload.action;
 
         switch (action) {
-            case 'opened':
-                await handlePROpened(slackToken, slackChannel, githubToken, initialMessageTemplate, commitListMessageTemplate, githubToSlackMapParsed);
-                break;
-            case 'synchronize':
-                await handlePRUpdated(slackToken, slackChannel, githubToken, updateMessageTemplate);
-                break;
-            case 'closed':
-                await handlePRClosed(slackToken, slackChannel, closeMessageTemplate);
-                break;
-            default:
-                throw new Error('Unsupported pull request event action');
+          case 'opened':
+            await handlePROpened(
+              slackToken,
+              slackChannel,
+              githubToken,
+              initialMessageTemplate,
+              githubToSlackMapParsed
+            );
+            break;
+          case 'synchronize':
+            await handlePRUpdated(
+              slackToken,
+              slackChannel,
+              githubToken,
+              updateMessageTemplate
+            );
+            break;
+          case 'closed':
+            await handlePRClosed(
+              slackToken,
+              slackChannel,
+              closeMessageTemplate
+            );
+            break;
+          default:
+            throw new Error('Unsupported pull request event action');
         }
     } catch (error) {
         if (error instanceof Error) {
