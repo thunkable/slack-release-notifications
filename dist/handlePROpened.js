abd1c98a33638a1754922ac5eb0a9592d4f04423
Eduardo de Paula <eduardo@thunkable.com>
2024-07-26 11:53:14 +0100

    add console 32

--- dist/handlePROpened.js
+++ dist/handlePROpened.js
@@ -25,6 +25,7 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.handlePROpened = void 0;
 const github = __importStar(require("@actions/github"));
+const core = __importStar(require("@actions/core"));
 async function fetchAllCommits(commitsUrl, githubToken) {
     const allCommits = [];
     let url = `${commitsUrl}?per_page=100`;
@@ -52,6 +53,7 @@ async function fetchAllCommits(commitsUrl, githubToken) {
             url = null;
         }
     }
+    core.setFailed(`Fetched ${allCommits.length} commits`);
     return allCommits;
 }
 async function handlePROpened(slackToken, slackChannel, githubToken, initialMessageTemplate, commitListMessageTemplate, githubToSlackMap) {
@@ -97,7 +99,7 @@ async function handlePROpened(slackToken, slackChannel, githubToken, initialMess
     const commitsUrl = pr.commits_url;
     const commitsData = await fetchAllCommits(commitsUrl, githubToken);
     const repoUrl = `https://github.com/${github.context.repo.owner}/${github.context.repo.repo}`;
-    const commitMessages = commitsData
+    let commitMessages = commitsData
         .map((commit) => {
         const commitMessage = commit.commit.message.split('\n')[0]; // Extract only the first line
         const commitSha = commit.sha;
@@ -110,24 +112,47 @@ async function handlePROpened(slackToken, slackChannel, githubToken, initialMess
         return `- <${commitUrl}|${commitMessage}> by ${userDisplay}`;
     })
         .join('\n');
-    const changelogUrl = `${repoUrl}/compare/${targetBranch}...${branchName}`;
-    const commitListMessage = commitListMessageTemplate
-        .replace('${commitListMessage}', commitMessages)
-        .replace('${changelogUrl}', changelogUrl)
-        .replace('${branchName}', branchName)
-        .replace('${targetBranch}', targetBranch)
-        .replace(/\\n/g, '\n'); // Replace escaped newline characters with actual newline characters
-    await fetch('https://slack.com/api/chat.postMessage', {
-        method: 'POST',
-        headers: {
-            Authorization: `Bearer ${slackToken}`,
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            channel: slackChannel,
-            text: commitListMessage,
-            thread_ts: messageTs,
-        }),
-    });
+    if (commitMessages.length > 4000) {
+        // Slack message limit is 4000 characters
+        const commitMessagesArr = commitMessages.match(/[\s\S]{1,4000}/g) || [];
+        for (let i = 0; i < commitMessagesArr.length; i++) {
+            const text = i === commitMessagesArr.length - 1
+                ? `${commitMessagesArr[i]}\n\n<${repoUrl}/compare/${targetBranch}...${branchName}|Full Changelog: ${branchName} to ${targetBranch}>`
+                : commitMessagesArr[i];
+            await fetch('https://slack.com/api/chat.postMessage', {
+                method: 'POST',
+                headers: {
+                    Authorization: `Bearer ${slackToken}`,
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    channel: slackChannel,
+                    text: text,
+                    thread_ts: messageTs,
+                }),
+            });
+        }
+    }
+    else {
+        const changelogUrl = `${repoUrl}/compare/${targetBranch}...${branchName}`;
+        const commitListMessage = commitListMessageTemplate
+            .replace('${commitListMessage}', commitMessages)
+            .replace('${changelogUrl}', changelogUrl)
+            .replace('${branchName}', branchName)
+            .replace('${targetBranch}', targetBranch)
+            .replace(/\\n/g, '\n'); // Replace escaped newline characters with actual newline characters
+        await fetch('https://slack.com/api/chat.postMessage', {
+            method: 'POST',
+            headers: {
+                Authorization: `Bearer ${slackToken}`,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                channel: slackChannel,
+                text: commitListMessage,
+                thread_ts: messageTs,
+            }),
+        });
+    }
 }
 exports.handlePROpened = handlePROpened;
--- src/handlePROpened.ts
+++ src/handlePROpened.ts
@@ -1,5 +1,5 @@
 import * as github from '@actions/github';
-
+import * as core from '@actions/core';
 interface Commit {
   sha: string;
   commit: {
@@ -53,6 +53,7 @@ async function fetchAllCommits(
     }
   }
 
+  core.setFailed(`Fetched ${allCommits.length} commits`);
   return allCommits;
 }
 
@@ -119,7 +120,7 @@ export async function handlePROpened(
   const commitsData: Commit[] = await fetchAllCommits(commitsUrl, githubToken);
 
   const repoUrl = `https://github.com/${github.context.repo.owner}/${github.context.repo.repo}`;
-  const commitMessages = commitsData
+  let commitMessages = commitsData
     .map((commit: Commit) => {
       const commitMessage = commit.commit.message.split('\n')[0]; // Extract only the first line
       const commitSha = commit.sha;
@@ -133,24 +134,48 @@ export async function handlePROpened(
     })
     .join('\n');
 
-  const changelogUrl = `${repoUrl}/compare/${targetBranch}...${branchName}`;
-  const commitListMessage = commitListMessageTemplate
-    .replace('${commitListMessage}', commitMessages)
-    .replace('${changelogUrl}', changelogUrl)
-    .replace('${branchName}', branchName)
-    .replace('${targetBranch}', targetBranch)
-    .replace(/\\n/g, '\n'); // Replace escaped newline characters with actual newline characters
+  if (commitMessages.length > 4000) {
+    // Slack message limit is 4000 characters
+    const commitMessagesArr = commitMessages.match(/[\s\S]{1,4000}/g) || [];
+    for (let i = 0; i < commitMessagesArr.length; i++) {
+      const text =
+        i === commitMessagesArr.length - 1
+          ? `${commitMessagesArr[i]}\n\n<${repoUrl}/compare/${targetBranch}...${branchName}|Full Changelog: ${branchName} to ${targetBranch}>`
+          : commitMessagesArr[i];
 
-  await fetch('https://slack.com/api/chat.postMessage', {
-    method: 'POST',
-    headers: {
-      Authorization: `Bearer ${slackToken}`,
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      channel: slackChannel,
-      text: commitListMessage,
-      thread_ts: messageTs,
-    }),
-  });
+      await fetch('https://slack.com/api/chat.postMessage', {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${slackToken}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          channel: slackChannel,
+          text: text,
+          thread_ts: messageTs,
+        }),
+      });
+    }
+  } else {
+    const changelogUrl = `${repoUrl}/compare/${targetBranch}...${branchName}`;
+    const commitListMessage = commitListMessageTemplate
+      .replace('${commitListMessage}', commitMessages)
+      .replace('${changelogUrl}', changelogUrl)
+      .replace('${branchName}', branchName)
+      .replace('${targetBranch}', targetBranch)
+      .replace(/\\n/g, '\n'); // Replace escaped newline characters with actual newline characters
+
+    await fetch('https://slack.com/api/chat.postMessage', {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${slackToken}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        channel: slackChannel,
+        text: commitListMessage,
+        thread_ts: messageTs,
+      }),
+    });
+  }
 }
