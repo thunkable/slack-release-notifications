9733ad0b2c72114d581803cdde520dcb628c34c2
Eduardo de Paula <eduardo@thunkable.com>
2024-07-26 16:39:32 +0100

    remove dist/index.js

--- src/main.ts
+++ src/main.ts
@@ -5,39 +5,55 @@ import { handlePRUpdated } from './handlePRUpdated';
 import { handlePRClosed } from './handlePRClosed';
 
 async function run() {
-    try {
-        const slackToken = core.getInput('slack-bot-token');
-        const slackChannel = core.getInput('slack-channel');
-        const githubToken = core.getInput('github-token');
-        const initialMessageTemplate = core.getInput('initial-message-template');
-        const updateMessageTemplate = core.getInput('update-message-template');
-        const closeMessageTemplate = core.getInput('close-message-template');
-        const commitListMessageTemplate = core.getInput('commit-list-message-template');
-        const githubToSlackMap = core.getInput('github-to-slack-map');
-        const githubToSlackMapParsed = githubToSlackMap ? JSON.parse(githubToSlackMap) : undefined;
+  try {
+    const slackToken = core.getInput('slack-bot-token');
+    const slackChannel = core.getInput('slack-channel');
+    const githubToken = core.getInput('github-token');
+    const initialMessageTemplate = core.getInput('initial-message-template');
+    const updateMessageTemplate = core.getInput('update-message-template');
+    const closeMessageTemplate = core.getInput('close-message-template');
+    const commitListMessageTemplate = core.getInput(
+      'commit-list-message-template'
+    );
+    const githubToSlackMap = core.getInput('github-to-slack-map');
+    const githubToSlackMapParsed = githubToSlackMap
+      ? JSON.parse(githubToSlackMap)
+      : undefined;
 
-        const action = github.context.payload.action;
+    const action = github.context.payload.action;
 
-        switch (action) {
-            case 'opened':
-                await handlePROpened(slackToken, slackChannel, githubToken, initialMessageTemplate, commitListMessageTemplate, githubToSlackMapParsed);
-                break;
-            case 'synchronize':
-                await handlePRUpdated(slackToken, slackChannel, githubToken, updateMessageTemplate);
-                break;
-            case 'closed':
-                await handlePRClosed(slackToken, slackChannel, closeMessageTemplate);
-                break;
-            default:
-                throw new Error('Unsupported pull request event action');
-        }
-    } catch (error) {
-        if (error instanceof Error) {
-            core.setFailed(error.message);
-        } else {
-            core.setFailed('An unknown error occurred');
-        }
+    switch (action) {
+      case 'opened':
+        await handlePROpened(
+          slackToken,
+          slackChannel,
+          githubToken,
+          initialMessageTemplate,
+          commitListMessageTemplate,
+          githubToSlackMapParsed
+        );
+        break;
+      case 'synchronize':
+        await handlePRUpdated(
+          slackToken,
+          slackChannel,
+          githubToken,
+          updateMessageTemplate
+        );
+        break;
+      case 'closed':
+        await handlePRClosed(slackToken, slackChannel, closeMessageTemplate);
+        break;
+      default:
+        throw new Error('Unsupported pull request event action');
     }
+  } catch (error) {
+    if (error instanceof Error) {
+      core.setFailed(error.message);
+    } else {
+      core.setFailed('An unknown error occurred');
+    }
+  }
 }
 
 run();
